9dd0227f238e283d188513c726fc76cf84298e6b
ahsan133 <mahsanullah512@gmail.com>
2020-09-15 23:25:14 +0530

    [MD Ahsanullah] Add: another CSV file is loaded

---
+++ CensusAnalyser/CensusAnalyser/CSVStates.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace CensusAnalyser
+{
+    public class CSVStates
+    {
+        public static int GetRecord(string path)
+        {
+            int count = 0;
+            string[] CensusData = File.ReadAllLines(path);
+            foreach (var data in CensusData)
+            {
+                count++;
+            }
+            return count - 1;
+        }
+    }
+}
