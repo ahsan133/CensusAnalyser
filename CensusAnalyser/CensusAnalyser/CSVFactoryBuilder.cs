d029923372b663a11f8f141ae73424d0e32f5588
ahsan133 <mahsanullah512@gmail.com>
2020-09-16 00:35:59 +0530

    [MD Ahsanullah] Refactor: test cases refactored and introduced CSV builder and factory

---
+++ CensusAnalyser/CensusAnalyser/CSVFactoryBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyser
+{
+    public class CSVFactoryBuilder : ICSVBuilder
+    {
+        public void CheckForException(char exceptionType)
+        {
+            switch (exceptionType)
+            {
+                case 'H':
+                    throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.HEADER_NOT_MATCH, "Invalid Header");
+                case 'W':
+                    throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.INVALID_FILEPATH, "Invalid File Path");
+                case 'N':
+                    throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.INCORRECT_FILETYPE, "Invalid File Name");
+                case 'D':
+                    throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.DELIMITER_INCORRECT, "Delimiter Incorrect");
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ CensusAnalyser/CensusAnalyser/ICSVBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyser
+{
+    public interface ICSVBuilder
+    {
+        public void CheckForException(char exceptionType);
+    }
+}
