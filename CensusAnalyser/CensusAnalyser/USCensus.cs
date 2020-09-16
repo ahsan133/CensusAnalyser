8146e825b82f0511b0db474a6b26a02011f50c78
ahsan133 <mahsanullah512@gmail.com>
2020-09-16 15:35:24 +0530

    [MD Ahsanullah] Add: US Census File is loaded

---
+++ CensusAnalyser/CensusAnalyser/USCensus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CensusAnalyser
+{
+    public class USCensus
+    {
+        public static void GetUSCensusData(string path)
+        {
+            if (!path.EndsWith(".csv"))
+            {
+                throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.INCORRECT_FILETYPE, "Invalid File Name");
+            }
+
+            string[] CensusData = File.ReadAllLines(path);
+            foreach (var element in CensusData)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
